d712a3bd5699cbb0f14d183d483a59756270114d
s-jad <s_jad@web.de>
2024-04-16 20:54:07 +0200

    Texture larger, square

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -1,7 +1,17 @@
+use std::f32;
+
 pub(crate) const SCREEN_WIDTH: u32 = 1376;
 pub(crate) const SCREEN_HEIGHT: u32 = 768;
-pub(crate) const DISPATCH_SIZE_X: u32 = ((SCREEN_WIDTH as u32).saturating_add(32)) / 32;
-pub(crate) const DISPATCH_SIZE_Y: u32 = ((SCREEN_HEIGHT as u32).saturating_add(32)) / 32;
+pub(crate) const ASPECT_RATIO: u32 = SCREEN_WIDTH / SCREEN_HEIGHT;
+
+pub(crate) const DISPATCH_SIZE_X: u32 = ((SCREEN_WIDTH).saturating_add(32)) / 32;
+pub(crate) const DISPATCH_SIZE_Y: u32 = ((SCREEN_HEIGHT).saturating_add(32)) / 32;
+
+pub(crate) const TEXTURE_WIDTH: u32 = 2048;
+pub(crate) const TEXTURE_HEIGHT: u32 = 2048;
+
+pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((TEXTURE_WIDTH).saturating_add(32)) / 32;
+pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((TEXTURE_HEIGHT).saturating_add(32)) / 32;
 
 pub(crate) const TERRAIN_TEX_BUF_SIZE: usize =
-    SCREEN_WIDTH as usize * SCREEN_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
+    TEXTURE_WIDTH as usize * TEXTURE_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
