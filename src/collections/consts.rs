18afb468e653c4734d241b25ed2bff2941e034cb
s-jad <s_jad@web.de>
2024-04-19 09:49:54 +0200

    Removed unused consts

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -1,11 +1,5 @@
-use std::f32;
-
 pub(crate) const SCREEN_WIDTH: u32 = 1376;
 pub(crate) const SCREEN_HEIGHT: u32 = 768;
-pub(crate) const ASPECT_RATIO: u32 = SCREEN_WIDTH / SCREEN_HEIGHT;
-
-pub(crate) const DISPATCH_SIZE_X: u32 = ((SCREEN_WIDTH).saturating_add(32)) / 32;
-pub(crate) const DISPATCH_SIZE_Y: u32 = ((SCREEN_HEIGHT).saturating_add(32)) / 32;
 
 pub(crate) const TEXTURE_WIDTH: u32 = 2048;
 pub(crate) const TEXTURE_HEIGHT: u32 = 2048;
