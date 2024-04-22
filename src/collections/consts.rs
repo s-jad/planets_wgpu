fff80f815a49582d64a5d963c250af6d11b2c104
s-jad <s_jad@web.de>
2024-04-22 09:29:52 +0200

    Added FOV_RAD, CLIP_NEAR/FAR for calculating projection matrix

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -1,5 +1,9 @@
 pub(crate) const SCREEN_WIDTH: u32 = 1376;
 pub(crate) const SCREEN_HEIGHT: u32 = 768;
+pub(crate) const ASPECT: f32 = SCREEN_WIDTH as f32 / SCREEN_HEIGHT as f32;
+pub(crate) const FOV_RAD: f32 = 1.5708;
+pub(crate) const CLIP_NEAR: f32 = 0.1;
+pub(crate) const CLIP_FAR: f32 = 300.0;
 
 pub(crate) const TEXTURE_WIDTH: u32 = 2048;
 pub(crate) const TEXTURE_HEIGHT: u32 = 2048;
@@ -9,3 +13,4 @@ pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((TEXTURE_HEIGHT).saturating_add(32)
 
 pub(crate) const TERRAIN_TEX_BUF_SIZE: usize =
     TEXTURE_WIDTH as usize * TEXTURE_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
+pub(crate) const WAVE_TEX_BUF_SIZE: usize = TERRAIN_TEX_BUF_SIZE;
