58178ddc5a972e120115e911181a0df58bf190b9
s-jad <s_jad@web.de>
2024-04-23 12:00:32 +0200

    Added ice texture consts

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -1,16 +1,25 @@
 pub(crate) const SCREEN_WIDTH: u32 = 1376;
 pub(crate) const SCREEN_HEIGHT: u32 = 768;
 pub(crate) const ASPECT: f32 = SCREEN_WIDTH as f32 / SCREEN_HEIGHT as f32;
-pub(crate) const FOV_RAD: f32 = 1.5708;
+pub(crate) const FOV_RAD: f32 = 0.1708;
 pub(crate) const CLIP_NEAR: f32 = 0.1;
 pub(crate) const CLIP_FAR: f32 = 300.0;
 
-pub(crate) const TEXTURE_WIDTH: u32 = 2048;
-pub(crate) const TEXTURE_HEIGHT: u32 = 2048;
+pub(crate) const TERRAIN_TEXTURE_WIDTH: u32 = 2048;
+pub(crate) const TERRAIN_TEXTURE_HEIGHT: u32 = 2048;
 
-pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((TEXTURE_WIDTH).saturating_add(32)) / 32;
-pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((TEXTURE_HEIGHT).saturating_add(32)) / 32;
+pub(crate) const ICE_TEXTURE_WIDTH: u32 = 1024;
+pub(crate) const ICE_TEXTURE_HEIGHT: u32 = 512;
 
-pub(crate) const TERRAIN_TEX_BUF_SIZE: usize =
-    TEXTURE_WIDTH as usize * TEXTURE_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
+pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((TERRAIN_TEXTURE_WIDTH).saturating_add(32)) / 32;
+pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((TERRAIN_TEXTURE_HEIGHT).saturating_add(32)) / 32;
+
+pub(crate) const TERRAIN_TEX_BUF_SIZE: usize = TERRAIN_TEXTURE_WIDTH as usize
+    * TERRAIN_TEXTURE_HEIGHT as usize
+    * 4
+    * (std::mem::size_of::<f32>());
+
 pub(crate) const WAVE_TEX_BUF_SIZE: usize = TERRAIN_TEX_BUF_SIZE;
+
+pub(crate) const ICE_TEX_BUF_SIZE: usize =
+    ICE_TEXTURE_WIDTH as usize * ICE_TEXTURE_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
