958b86849011d01bd9482dac87d487296995cd5a
s-jad <s_jad@web.de>
2024-04-24 13:07:11 +0200

    Added 3 mip levels, removed ice texture consts - moved to .y of terrain_tex

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -8,18 +8,16 @@ pub(crate) const CLIP_FAR: f32 = 300.0;
 pub(crate) const TERRAIN_TEXTURE_WIDTH: u32 = 2048;
 pub(crate) const TERRAIN_TEXTURE_HEIGHT: u32 = 2048;
 
-pub(crate) const ICE_TEXTURE_WIDTH: u32 = 1024;
-pub(crate) const ICE_TEXTURE_HEIGHT: u32 = 512;
-
 pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((TERRAIN_TEXTURE_WIDTH).saturating_add(32)) / 32;
 pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((TERRAIN_TEXTURE_HEIGHT).saturating_add(32)) / 32;
 
-pub(crate) const TERRAIN_TEX_BUF_SIZE: usize = TERRAIN_TEXTURE_WIDTH as usize
+pub(crate) const MIP_LEVEL_0: usize = TERRAIN_TEXTURE_WIDTH as usize
     * TERRAIN_TEXTURE_HEIGHT as usize
     * 4
     * (std::mem::size_of::<f32>());
 
-pub(crate) const WAVE_TEX_BUF_SIZE: usize = TERRAIN_TEX_BUF_SIZE;
+pub(crate) const MIP_LEVEL_1: usize = MIP_LEVEL_0 / 2;
+pub(crate) const MIP_LEVEL_2: usize = MIP_LEVEL_1 / 2;
+pub(crate) const TERRAIN_TEX_BUF_SIZE: usize = MIP_LEVEL_0 + MIP_LEVEL_1 + MIP_LEVEL_2;
 
-pub(crate) const ICE_TEX_BUF_SIZE: usize =
-    ICE_TEXTURE_WIDTH as usize * ICE_TEXTURE_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
+pub(crate) const WAVE_TEX_BUF_SIZE: usize = TERRAIN_TEX_BUF_SIZE;
