a8956cbbfcefd97b00fee95c9bdc674f4a594347
s-jad <s_jad@web.de>
2024-04-25 17:52:07 +0200

    Removed mip levels (storage tex, didnt work), added moon tex consts

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -7,15 +7,19 @@ pub(crate) const CLIP_FAR: f32 = 300.0;
 
 pub(crate) const PLANET_TEXTURE_WIDTH: u32 = 2048;
 pub(crate) const PLANET_TEXTURE_HEIGHT: u32 = 2048;
+pub(crate) const MOON_TEXTURE_WIDTH: u32 = 512;
+pub(crate) const MOON_TEXTURE_HEIGHT: u32 = 512;
 
-pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((PLANET_TEXTURE_WIDTH).saturating_add(32)) / 32;
-pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((PLANET_TEXTURE_HEIGHT).saturating_add(32)) / 32;
+pub(crate) const PLANET_TEX_DISPATCH_SIZE_X: u32 = ((PLANET_TEXTURE_WIDTH).saturating_add(32)) / 32;
+pub(crate) const PLANET_TEX_DISPATCH_SIZE_Y: u32 =
+    ((PLANET_TEXTURE_HEIGHT).saturating_add(32)) / 32;
+pub(crate) const MOON_TEX_DISPATCH_SIZE_X: u32 = ((MOON_TEXTURE_WIDTH).saturating_add(32)) / 32;
+pub(crate) const MOON_TEX_DISPATCH_SIZE_Y: u32 = ((MOON_TEXTURE_HEIGHT).saturating_add(32)) / 32;
 
-pub(crate) const MIP_LEVEL_0: usize = PLANET_TEXTURE_WIDTH as usize
+pub(crate) const PLANET_TEX_BUF_SIZE: usize = PLANET_TEXTURE_WIDTH as usize
     * PLANET_TEXTURE_HEIGHT as usize
     * 4
     * (std::mem::size_of::<f32>());
 
-pub(crate) const MIP_LEVEL_1: usize = MIP_LEVEL_0 / 2;
-pub(crate) const MIP_LEVEL_2: usize = MIP_LEVEL_1 / 2;
-pub(crate) const PLANET_TEX_BUF_SIZE: usize = MIP_LEVEL_0 + MIP_LEVEL_1 + MIP_LEVEL_2;
+pub(crate) const MOON_TEX_BUF_SIZE: usize =
+    MOON_TEXTURE_WIDTH as usize * MOON_TEXTURE_HEIGHT as usize * 4 * (std::mem::size_of::<f32>());
