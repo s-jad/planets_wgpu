0691623ce1ea84ed01b972ce162f18b66d938dc1
s-jad <s_jad@web.de>
2024-04-25 17:24:21 +0200

    Renamed planet-related consts - easier to understand connection

--- src/collections/consts.rs
+++ src/collections/consts.rs
@@ -5,19 +5,17 @@ pub(crate) const FOV_RAD: f32 = 0.1708;
 pub(crate) const CLIP_NEAR: f32 = 0.1;
 pub(crate) const CLIP_FAR: f32 = 300.0;
 
-pub(crate) const TERRAIN_TEXTURE_WIDTH: u32 = 2048;
-pub(crate) const TERRAIN_TEXTURE_HEIGHT: u32 = 2048;
+pub(crate) const PLANET_TEXTURE_WIDTH: u32 = 2048;
+pub(crate) const PLANET_TEXTURE_HEIGHT: u32 = 2048;
 
-pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((TERRAIN_TEXTURE_WIDTH).saturating_add(32)) / 32;
-pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((TERRAIN_TEXTURE_HEIGHT).saturating_add(32)) / 32;
+pub(crate) const TEX_DISPATCH_SIZE_X: u32 = ((PLANET_TEXTURE_WIDTH).saturating_add(32)) / 32;
+pub(crate) const TEX_DISPATCH_SIZE_Y: u32 = ((PLANET_TEXTURE_HEIGHT).saturating_add(32)) / 32;
 
-pub(crate) const MIP_LEVEL_0: usize = TERRAIN_TEXTURE_WIDTH as usize
-    * TERRAIN_TEXTURE_HEIGHT as usize
+pub(crate) const MIP_LEVEL_0: usize = PLANET_TEXTURE_WIDTH as usize
+    * PLANET_TEXTURE_HEIGHT as usize
     * 4
     * (std::mem::size_of::<f32>());
 
 pub(crate) const MIP_LEVEL_1: usize = MIP_LEVEL_0 / 2;
 pub(crate) const MIP_LEVEL_2: usize = MIP_LEVEL_1 / 2;
-pub(crate) const TERRAIN_TEX_BUF_SIZE: usize = MIP_LEVEL_0 + MIP_LEVEL_1 + MIP_LEVEL_2;
-
-pub(crate) const WAVE_TEX_BUF_SIZE: usize = TERRAIN_TEX_BUF_SIZE;
+pub(crate) const PLANET_TEX_BUF_SIZE: usize = MIP_LEVEL_0 + MIP_LEVEL_1 + MIP_LEVEL_2;
